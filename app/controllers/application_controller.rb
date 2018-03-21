d117aa7af75b75ed7b7ce2bac2dce33458d88664
likys12 <likys12@outlook.com>
2018-03-21 22:19:52 +0800

    chore: stash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_date
+  def set_date
+  @date_loaded = Time.now.strftime "%Y/%d/%m %I:%M%p"
+  end
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -41,4 +41,23 @@ class ProductTest < ActiveSupport::TestCase
     assert new_product(name).invalid?,"#{name} shouldn't be valid"
     end  
   end
+  test "product is not valid without a unique title" do
+    product =Product.new(
+      title:    products(:ruby).title,
+      description:      "yyy",
+      price:      1,
+      image_url:      "fred.gif")
+    assert product.invalid?
+    assert_equal ["has already been taken"], product.errors[:title]
+  end
+  test "product is not valid without a unique title  - i18n " do
+    product =Product.new(
+      title:  products(:ruby).title,
+      description:  "yyy",
+      price:  1,
+      image_url:  "fred.gif")
+    assert product.invalird?
+    assert_equal [I18n.translate('errors.messages.taken')],
+                 product.errors[:title]
+  end
 end
